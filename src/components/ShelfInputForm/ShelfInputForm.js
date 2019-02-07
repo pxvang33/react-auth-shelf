11c4ea18d27e5c40d3f4f2cf3c95064135d2addb
mes32 <stockman.mike@gmail.com>
2019-02-07 15:36:32 -0600

    add form

--- src/components/ShelfInputForm/ShelfInputForm.js
+++ src/components/ShelfInputForm/ShelfInputForm.js
@@ -2,11 +2,28 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
 class ShelfInputForm extends Component {
+    constructor(props){
+        super(props);
+        this.state= {
+            description: '',
+            image_url: '',
+        }
+    }
+
+    submit = (event) => {
+        event.preventDefault();
+        console.log('submit');
+    }
 
     render() {
         return (
             <div>
                 <p>[ShelfInputForm]</p>
+                <form onSubmit={this.submit}>
+                    <input type="text" name="description" placeholder="description" required />
+                    <input type="text" name="image_url" placeholder="Image" required />
+                    <input type="submit" />
+                </form>
             </div>
         );
     }
